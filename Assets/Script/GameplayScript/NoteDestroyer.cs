5f6fb78bbb52fef844fbd117d7301a7259c12203
kyuuri <dark_e2102@hotmail.com>
2016-01-24 14:34:52 +0700

    Update gitignore, working on missing note update

---
+++ Assets/Script/GameplayScript/NoteDestroyer.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NoteDestroyer : MonoBehaviour {
+
+	public List<NoteDescription>[] allnotes;
+	private List<NoteDescription> laneNotes;
+
+	void Update () {
+		allnotes = NoteRenderer.allnotes;
+
+		for (int i = 0; i < allnotes.Length; i++) {
+			laneNotes = NoteRenderer.allnotes[i];
+			NoteDescription note = laneNotes [0];
+			float deltaTime = GetDeltaTime (note.HitTime);
+		}
+	}
+
+	private float GetDeltaTime(float noteTime){
+		float hitDeltaTime = noteTime - TimerScript.timePass;
+		hitDeltaTime *= 1000;
+		return hitDeltaTime;
+	}
+
+
+
+
+
+
+}
