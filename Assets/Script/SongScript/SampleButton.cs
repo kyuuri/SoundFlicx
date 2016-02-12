82d7101864b4e61a80d814796b7d6661c2090d0f
Aof Wat Wattanagaroon <wat.wattanagaroon@gmail.com>
2016-02-12 13:37:00 +0700

    More Code about song selection scene

--- Assets/Script/SongScript/SampleButton.cs
+++ Assets/Script/SongScript/SampleButton.cs
@@ -6,7 +6,10 @@ public class SampleButton : MonoBehaviour {
 
 	public Button button;
 	public Text nameLabel;
+	public Text composer;
 	public Texture2D icon;
+	public int bpm;
+	public int level;
 //	public Text typeLabel;
 //	public Text rarityLabel;
 //	public GameObject championIcon;
---
+++ Assets/Script/SongScript/SongSelectionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SongSelectionController : MonoBehaviour {
+
+	public Transform contentPanel;
+	public Transform speedPanel;
+	public Transform buttonLeft;
+	public Transform buttonRight;
+	private Vector3 destination = new Vector3(1000,1000,1000);
+
+	// Use this for initialization
+	void Start () {
+		speedPanel.position = Vector3.Slerp (speedPanel.position, destination,5);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
