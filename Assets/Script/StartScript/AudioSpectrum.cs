d6672e061411b7b1d00e730eec06686d00696f50
kyuuri <dark_e2102@hotmail.com>
2016-02-14 00:39:02 +0700

    Add wave indicator in the front page.

--- Assets/Script/StartScript/AudioSpectrum.cs
+++ Assets/Script/StartScript/AudioSpectrum.cs
@@ -3,16 +3,24 @@ using System.Collections;
 
 public class AudioSpectrum : MonoBehaviour {
 	public GameObject prefab;
-	public int numberOfObjects = 20;
+	public int numberOfObjects = 24;
 	public float radius = 5f;
+
 	public GameObject[] cube;
 
 	void Start() {
-		for (int i = 0; i < numberOfObjects; i++) {
-			float angle = i * Mathf.PI * 2 / numberOfObjects;
-			Vector3 pos = new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
-			Instantiate(prefab, pos, Quaternion.identity);
+		for (int i = 0; i < numberOfObjects/2; i++) {
+			Vector3 pos = new Vector3 ((i-numberOfObjects/2)/4.0f,0,0);
+			GameObject obj = (GameObject)Instantiate(prefab, pos, Quaternion.identity);
+			obj.GetComponent<Renderer> ().material.color = Color.HSVToRGB (i / ((numberOfObjects / 2.0f) - 1), 0.7f, 0.7f );
+
+			pos = new Vector3 (-(i-numberOfObjects/2)/4.0f,0,0);
+			obj = (GameObject)Instantiate(prefab, pos, Quaternion.identity);
+			obj.GetComponent<Renderer> ().material.color = Color.HSVToRGB (i / ((numberOfObjects / 2.0f) - 1), 0.7f, 0.7f );
+
+
 		}
+
 		cube = GameObject.FindGameObjectsWithTag ("cubes");
 	}
 
@@ -23,7 +31,7 @@ public class AudioSpectrum : MonoBehaviour {
 		float[] spectrum = AudioListener.GetSpectrumData (1024, 0, FFTWindow.Hamming);
 		for (int i = 0; i < numberOfObjects; i++) {
 			Vector3 previousScale = cube [i].transform.localScale;
-			previousScale.y = spectrum [i] * 20;
+			previousScale.y = spectrum [i] * 40;
 			cube [i].transform.localScale = previousScale;
 		}
 	}
