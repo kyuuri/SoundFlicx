065d5bf160333f611ee853309a3c52289c3a9eb4
kyuuri <dark_e2102@hotmail.com>
2016-02-14 02:20:41 +0700

    Edit TCBS level data

--- Assets/Script/GameplayScript/NoteRenderer.cs
+++ Assets/Script/GameplayScript/NoteRenderer.cs
@@ -40,7 +40,7 @@ public class NoteRenderer : MonoBehaviour {
 		if (songName == "Test") {
 			events = noteData.midiEvents [Difficulty.NORMAL];
 		} else {
-			events = noteData.midiEvents [Difficulty.HARD];
+			events = noteData.midiEvents [GlobalData.selectedTrack.difficulty];
 		}
 
 		for (int i = 0; i < events.Count ; i++) {
