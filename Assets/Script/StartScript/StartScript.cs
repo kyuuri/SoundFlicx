c73f3f7fc45d5f53df4934a3e35748742f1ab407
kyuuri <dark_e2102@hotmail.com>
2016-02-13 22:06:58 +0700

    Decorate start scene

--- Assets/Script/StartScript/StartScript.cs
+++ Assets/Script/StartScript/StartScript.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 using Leap;
 
 public class StartScript : MonoBehaviour {
+
+	public Text startText;
+	private static float delay = 0.5f;
+	private float timer = 0;
+	private bool blinkUp = false;
+	private float opac = 1.0f;
+
+	private bool goingNext = false;
+	private float sceneTimer = 0;
+
 	Leap.Controller controller;
 	// Use this for initialization
 	void Start () {
@@ -12,38 +23,46 @@ public class StartScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		Blink ();
+
 		Leap.Frame frame = controller.Frame ();
 		GestureList gestures = frame.Gestures();
 
 		for (int i = 0; i < gestures.Count; i++) {
 			if (gestures [i].Type == Gesture.GestureType.TYPE_SWIPE) {
-				ChangeScene ();
+				goingNext = true;
+				delay = 0.05f;
 			}
 		}
 
+		if(goingNext){
+			sceneTimer += Time.deltaTime;
+		}
+
+		if (sceneTimer > 1.6f) {
+			ChangeScene ();
+		}
+	}
+
+	void Blink(){
+		//startText.enabled = blinkUp;
+		if (blinkUp) {
+			opac += 0.05f;
+		} else {
+			opac -= 0.05f;
+		}
+		startText.color = new Color (startText.color.r, startText.color.g, startText.color.b, opac);
+		timer += Time.deltaTime;
+
+		if (timer >= delay) {
+			timer = 0;
+			blinkUp = !blinkUp;
+		}
 	}
 
 	public void ChangeScene(){
-		Debug.Log("Change Sceen");
 		Application.LoadLevel("SongSelection");
 	}
-
-	//	void OnGUI(){
-
-
-	//		if(GUI.Button(new Rect( PercentWidth(10), PercentHeight(5), 100, 20), "Display 10")){
-	//			Debug.Log ("On GUI 1");
-	////			textMesh.text = "10";
-	//
-	//		}
-
-	//		if (GUI.Button (new Rect (10, 90, 100, 20), button)) {
-	//			Debug.Log ("On GUI 2");
-	////			textMesh.text = "5";
-	//
-	//		}
-
-	//	}
 
 	float PercentWidth(float width){
 		return (width / 100) * UnityEngine.Screen.width;
