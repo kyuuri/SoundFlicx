20b76e94f1358f9f895f3ae8ceae1ef96e1969a1
kyuuri <dark_e2102@hotmail.com>
2016-03-10 14:22:32 +0700

    Add new Hit Effect.

--- Assets/Script/GameplayScript/LineHitChecker.cs
+++ Assets/Script/GameplayScript/LineHitChecker.cs
@@ -15,28 +15,84 @@ public class LineHitChecker : MonoBehaviour {
 	private float score;
 	private List<NoteDescription> laneNotes;
 
+	private Vector3 initScale;
+	private Vector3 initPivotScale;
+
+	private bool isShrinking;
+	private float delayShrink = 0.4f;
+	private float counterShrink = 0;
+
+	private bool release = false;
+
+	private bool isDrawing;
+	private float delayDraw = 0.4f;
+	private float counterDraw = 0;
+
 	public LaneHitState laneState = LaneHitState.NONE;
 
 	void Start () {
+		release = false;
 		Vector3 pos = lineChecker.transform.localPosition;
+
 		firstPosition = new Vector3 (pos.x, pos.y, pos.z);
+
+		initScale = lineChecker.transform.localScale;
+		initPivotScale = transform.localScale;
+
+		transform.localScale = new Vector3(0.00001f , initPivotScale.y, initPivotScale.z);
+	}
+
+	void ShrinkEffect(){
 		Vector3 scale = lineChecker.transform.localScale;
+		//if(scale.x > 0.000001f){
+		if(scale.x > 0.000001f){
+			lineChecker.transform.localScale = new Vector3 (scale.x * 0.25f, scale.y, scale.z);
+		}
+
+		scale = transform.localScale;
+
+		if(scale.z > 1){
+		//if(counterDraw < counterDraw){
+			transform.localScale = new Vector3 (initPivotScale.x, initPivotScale.y, scale.z * 0.8f);
+		}
+	}
+
+	void DrawEffect(){
+		Vector3 scale = transform.localScale;
+		//if(scale.z < 8){
+		if(scale.z < 8){
+			transform.localScale = new Vector3 (initPivotScale.x, initPivotScale.y, scale.z * 2.1f);
+		}
 	}
 
 	// Update is called once per frame
 	void Update () {
-
-		Vector3 pos = lineChecker.transform.position;
 		if (Input.GetKeyDown(key)) {
+			release = false;
 			//Debug.Log (key);
-			Vector3 hidePosition = new Vector3 (firstPosition.x, firstPosition.y + 2.7f, 0);
-			lineChecker.transform.localPosition = hidePosition;
+
+			lineChecker.transform.localScale = initScale;
+
+			Vector3 scale = transform.localScale;
+			transform.localScale = new Vector3(scale.x, scale.y, 1);
+
+			isShrinking = false;
+			isDrawing = true;
+
+			counterShrink = 0;
+			counterDraw = 0;
+
+			//Vector3 hidePosition = new Vector3 (firstPosition.x, firstPosition.y + 2.7f, 0);
+			//lineChecker.transform.localPosition = hidePosition;
 
 			laneState = LaneHitState.HIT;
 		}
 		else if(Input.GetKeyUp (key)){
-			Vector3 hidePosition = new Vector3 (firstPosition.x, firstPosition.y - 2.7f, 0);
-			lineChecker.transform.localPosition = hidePosition;
+			release = true;
+			isShrinking = true;
+			//isDrawing = false;
+			//Vector3 hidePosition = new Vector3 (firstPosition.x, firstPosition.y - 2.7f, 0);
+			//lineChecker.transform.localPosition = hidePosition;
 			laneState = LaneHitState.NONE;
 		}
 			
@@ -44,6 +100,23 @@ public class LineHitChecker : MonoBehaviour {
 
 		if (Input.GetKey(key)) {
 			laneState = LaneHitState.HOLD;
+		}
+
+		if (counterDraw >= delayDraw) {
+			isDrawing = false;
+		}
+		if (counterShrink >= delayShrink) {
+			isShrinking = true;
+		}
+
+		if (isDrawing) {
+			counterDraw += Time.deltaTime;
+			DrawEffect ();
+		}
+
+		if (isShrinking) {
+			counterShrink += Time.deltaTime;
+			ShrinkEffect ();
 		}
 	}
 
