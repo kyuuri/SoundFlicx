88b75c92568a975f8d95cec18afb18c4b33748dc
Aof Wat Wattanagaroon <wat.wattanagaroon@gmail.com>
2016-04-11 15:47:19 +0700

    Update create name scene and controller

---
+++ Assets/Script/CreateName/TypeController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TypeController : MonoBehaviour {
+
+	public Text text;
+	// Use this for initialization
+	void Start () {
+		text.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void AddText (string label){
+		text.text = text.text + label;
+	}
+
+	public void DeleteLabel(){
+		Debug.Log (text.text.Length);
+		if(text.text.Length>0)
+			text.text = text.text.Remove (text.text.Length-1);
+	}
+}
